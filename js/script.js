aca74aa5f78038c63d4ecc994d366310cc63e11f
M Michalczyk <pan.michalczyk@gmail.com>
2018-08-06 21:14:22 +0200

    Add Main jumbotron content, add Social media, finish Main-jumbotron

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 'use strict';
 
-// Main jumbotron - navigation - hide logo in burger menu
+// Main jumbotron - navigation - hide logo & content after clicked burger menu
 var burger = document.querySelector('.menu'),
-    logo = document.querySelector('.hide-logo');
-
+    logo = document.querySelector('.hide-logo'),
+    mJumbotronContent = document.querySelector('.main-jumbotron-content');
+console.log(logo);
+console.log(mJumbotronContent);
 burger.addEventListener('click', function(event) {
+    
+    // Hide logo
     logo.classList.toggle('hide-menu');
+    
+    // Hide content
+    mJumbotronContent.classList.toggle('bolek');
 })
