c0c50bf3fa7e64c39c9c0f11558334e2425407d4
M Michalczyk <pan.michalczyk@gmail.com>
2018-08-06 22:23:30 +0200

    Add meta for mobiles, fix bugs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
 'use strict';
 
-// Main jumbotron - navigation - hide logo & content after clicked burger menu
+// Main jumbotron - navigation - hide logo after clicked burger menu
 var burger = document.querySelector('.menu'),
-    logo = document.querySelector('.hide-logo'),
-    mJumbotronContent = document.querySelector('.main-jumbotron-content');
-console.log(logo);
-console.log(mJumbotronContent);
+    logo = document.querySelector('.hide-logo');
+
 burger.addEventListener('click', function(event) {
     
     // Hide logo
     logo.classList.toggle('hide-menu');
-    
-    // Hide content
-    mJumbotronContent.classList.toggle('bolek');
 })
